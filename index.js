a00207405e88a3ce1477a2821b21d6bd852b9cff
Philip Ng <phil__n@hotmail.com>
2020-03-28 04:28:05 -0400

    added color coding for time blocks based on past/present/future

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const now = moment().startOf("hour");
 $("#date").text(now.format("DD MMM YYYY"));
-const list = { 9: "sdfsdf", 10: "sdlkf" };
+const present = now.hour();
 
 const nine = moment()
   .hour(9)
@@ -9,17 +9,37 @@ const toFive = moment()
   .hour(17)
   .startOf("hour");
 
+let todolist;
+
+const storedToDos = localStorage.getItem("to-do");
+if (storedToDos !== null) {
+  todolist = JSON.parse(storedToDos);
+} else {
+  todolist = {};
+}
+
 for (let hour = nine.clone(); hour.isSameOrBefore(toFive); hour.add(1, "h")) {
   const clonedList = $("#clonedDiv").clone();
   clonedList.removeAttr("id");
-  clonedList.attr("data-index", hour.hour());
-  clonedList.find("#hour").text(hour.format("HA"));
-  $(".todo-section").append(clonedList);
-}
+  const currentTime = hour.hour();
+  clonedList.attr("data-index", currentTime);
+  const thisHour = hour.format("HA");
+  clonedList.find("#hour").text(thisHour);
 
-const storedTodos = localStorage.getItem(list);
-if (storedTodos !== null) {
-  list = JSON.parse(storedTodos);
+  if (currentTime < present) {
+    $("#hour").addClass("past");
+  }
+  if (currentTime === present) {
+    $("#hour").addClass("present");
+  }
+  if (currentTime > present) {
+    $("#hour").addClass("future");
+  }
+
+  if (todolist[hour] !== undefined) {
+    clonedList.find(".inputBox").val(todolist[hour]);
+  }
+  $(".todo-section").append(clonedList);
 }
 
 $(".todo-section").on("click", "#saveBtn", function() {
@@ -28,12 +48,12 @@ $(".todo-section").on("click", "#saveBtn", function() {
     .attr("data-index");
   const userTodo = $(this)
     .parent()
-    .find(".inputBox")
+    .find("#inputBox")
     .val();
 
-  list[hour] = userTodo;
+  todolist[hour] = userTodo;
 
-  console.log(list);
+  console.log(todolist);
 
-  localStorage.setItem("to-do", JSON.stringify(list));
+  localStorage.setItem("to-do", JSON.stringify(todolist));
 });
