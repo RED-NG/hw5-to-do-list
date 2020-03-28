29743c9b4b3952c8c953b70d0bd507bbc83abf31
Philip Ng <phil__n@hotmail.com>
2020-03-28 03:05:30 -0400

    clicking save button saves hour and value of user input into local storage

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const now = moment().startOf("hour");
 $("#date").text(now.format("DD MMM YYYY"));
+const list = { 9: "sdfsdf", 10: "sdlkf" };
 
 const nine = moment()
   .hour(9)
@@ -16,9 +17,23 @@ for (let hour = nine.clone(); hour.isSameOrBefore(toFive); hour.add(1, "h")) {
   $(".todo-section").append(clonedList);
 }
 
+const storedTodos = localStorage.getItem(list);
+if (storedTodos !== null) {
+  list = JSON.parse(storedTodos);
+}
+
 $(".todo-section").on("click", "#saveBtn", function() {
   const hour = $(this)
     .parent()
     .attr("data-index");
-  console.log(hour);
+  const userTodo = $(this)
+    .parent()
+    .find(".inputBox")
+    .val();
+
+  list[hour] = userTodo;
+
+  console.log(list);
+
+  localStorage.setItem("to-do", JSON.stringify(list));
 });
